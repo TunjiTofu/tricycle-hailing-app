08ae5b8f93d41bcbbeefe51e3adab2f83863896c
TunjiTofu <adetunjitofunmi@gmail.com>
2023-02-14 09:55:22 +0100

    Google Map API Snippets

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -6,15 +6,10 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Keke extends Model
+class Place extends Model
 {
     use HasFactory;
     use HasUuids;
 
     protected $guarded = [];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class,'rider_id','id');
-    }
 }
